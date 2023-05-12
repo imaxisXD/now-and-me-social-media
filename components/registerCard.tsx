3b3e7030f5c56b70d34736ea7bca8767395a7f35
imaxisXD <sunny735084@gmail.com>
2023-05-12 12:17:31 +0530

    Done: Register Card

--- components/registerCard.tsx
+++ components/registerCard.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import Image from "next/image"
+import Link from "next/link"
+import { ChangeEvent, useState } from "react"
 import { Button } from "./ui/button"
 import {
     Card,
@@ -13,44 +15,94 @@ import {
 import { Input } from "./ui/input"
 import { Label } from "./ui/label"
 
+interface RegisterCardProps {
+    toggleHandler: () => void
+}
 
-export function LoginCard() {
+export function RegisterCard({ toggleHandler }: RegisterCardProps) {
+    const [user, setUser] = useState("");
+    const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+
+    function handleShowPassword() {
+        setShowPassword(prev => !prev);
+    }
+    function handleRegister() {
+        if (user && password) {
+            registerCaller();
+        }
+    }
+
+    function handleInputChange(e: ChangeEvent<HTMLInputElement>) {
+        const { name, value } = e.target;
+        console.log(name, value);
+        switch (name) {
+            case 'email':
+                setUser(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            default:
+                break;
+        }
+    }
+    async function registerCaller() {
+        const data = { username: user, password: password }
+        try {
+            const response = await fetch('/api/register', {
+                method: "POST",
+                body: JSON.stringify(data),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+            }
+            else {
+                throw new Error('Request failed with status ' + response.status);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
     return (
-        <Card className="w-full md:w-3/4 max-w-md h-2/4 max-h-md border-none bg-gradient-to-br p-0.5 from-[#969696] to-[#343434]">
+        <Card className="w-full md:w-3/4 max-w-md h-fit max-h-md border-none bg-gradient-to-br p-0.5 from-[#969696] to-[#343434]">
             <div className="bg-[#27292D] rounded-lg">
                 <CardHeader className="space-y-1 text-center">
-                    <CardTitle className="text-sm font-normal text-greyed tracking-wider mt-3 mb-0.5">WELCOME BACK</CardTitle>
+                    <CardTitle className="text-sm font-normal text-greyed tracking-wider mt-3 mb-0.5">SIGN UP</CardTitle>
                     <CardDescription className="text-lg font-medium text-white">
-                        Log into your account
+                        Create an account to continue
                     </CardDescription>
                 </CardHeader>
                 <CardContent className="grid gap-4 mt-5">
                     <div className="grid gap-2 ">
-                        <Label htmlFor="email" className="text-[#C5C7CA]">Email or Username</Label>
-
-                        <Input id="email" type="email" className="placeholder:text-[#7F8084] border-[#35373B]" placeholder="Enter your email or username" />
+                        <Label htmlFor="email" className="text-[#C5C7CA]">Email</Label>
+                        <Input required id="email" name='email' type="email" className="placeholder:text-[#7F8084] border-[#35373B]" placeholder="Enter your email" onInput={handleInputChange} />
+                    </div>
+                    <div className="grid gap-2 ">
+                        <Label htmlFor="username" className="text-[#C5C7CA]">Username</Label>
+                        <Input required id="username" name='username' type="text" className="placeholder:text-[#7F8084] border-[#35373B]" placeholder="Choose a preferred username" onInput={handleInputChange} />
                     </div>
                     <div className="grid gap-2">
                         <div className="flex justify-between items-center">
                             <Label htmlFor="password" className="text-[#C5C7CA] inline-block">Password</Label>
-                            <span className="text-[#C5C7CA] inline text-xs">Forgot password?</span>
                         </div>
                         <div className="relative">
-                            <Input id="password" type="password" className="placeholder:text-[#7F8084] border-[#35373B] " placeholder="Enter your password" />
+                            <Input required name="password" id="password" type={showPassword ? 'text' : 'password'} className="placeholder:text-[#7F8084] border-[#35373B] " placeholder="Choose a strong password" onInput={handleInputChange} />
                             <Image width={20} height={20}
                                 className="absolute top-1/2 transform -translate-y-1/2 right-4"
                                 alt="show password"
                                 src="/eye.svg"
+                                onClick={handleShowPassword}
                             />
                         </div>
                     </div>
-                    <Button className="w-full bg-btnBlue text-base font-normal	">Login now</Button>
+                    <Button className="w-full bg-btnBlue text-base font-normal" onClick={handleRegister}>Continue</Button>
                 </CardContent>
                 <CardFooter className="text-xs font-medium">
-                    <span className="text-[#7F8084]">Not registered yet?<span className="text-[#C5C7CA]"> Register →</span></span>
+                    <span className="text-[#7F8084]">Already have an account?<span className="text-[#C5C7CA]" onClick={toggleHandler}> Login →</span></span>
                 </CardFooter>
             </div>
-
         </Card>
     )
 }
